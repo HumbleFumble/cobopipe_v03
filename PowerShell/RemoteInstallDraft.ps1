ddc8cdd352bbf68a9baf5c4170cfa3f76dd83f5f
n00bcybot <freshmaker21@gmail.com>
2023-01-24 16:52:36 +0100

    Starting Remote Install script

---
+++ PowerShell/RemoteInstallDraft.ps1
@@ -0,0 +1,65 @@
+
+
+# Get / Set connection profile on the target machine. 
+# Once a computer joins a domain, the network profile changes automatically to "Domain"
+# ----------------------------------------------------------------------------------------------------------------------
+# Get network profile
+(Get-NetConnectionProfile).NetworkCategory
+# Set network profile
+Set-NetConnectionProfile -NetworkCategory "Private"
+
+# Enabling / disabling "File And Printer Sharing" and "Network Discovery"
+# ----------------------------------------------------------------------------------------------------------------------
+# Enabling "File And Printer Sharing" is necessary for communication between 2 computers on a network
+# For connection to be established between two Windows machines, both machines must have "File And Printer Sharing" enabled
+# If the machines are on domain, "File And Printer Sharing" must be enabled for the "Domain" network profile
+# For rest of the profiles "File And Printer Sharing" can remain turned off as it is not necessary to be 
+# on for remote operations in a domain
+# "Network Discovery" only makes the computer visible on the network, but not accessible
+
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Profile "Domain" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Network Discovery" -Profile "Domain" -Enabled True
+
+# Check if "File and printer sharing" is enabled for the "Domain" profile and enable it if it's not
+if ((Get-NetFirewallRule -DisplayGroup "File and printer sharing").Disabled){
+    Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Profile "Domain" -Enabled True
+}
+
+# Hashtable containing applications intended for instalation and the respective command
+$apps = @{firefox = @{firefox.exe = "/S"}}
+
+# Installing application. It appears that the application must have option for silent install to be installed remotely.
+$creds = Get-Credential -UserName "network\admin"
+$drive_root = "\\driveroot\folder"
+$drive_name = "drive letter"
+$computer_name = 'computer name'
+$appName = '\' + $apps['firefox'].Keys
+$install_app = $drive_root + $appName
+$appArgs = $apps['firefox'].Values
+
+Invoke-Command -ComputerName $computer_name -ScriptBlock {
+    # Map the network share within PowerShell
+    New-PSDrive -Name $using:drive_name -Root $using:drive_root -PSProvider "FileSystem" -Credential $using:creds -Persist
+    # The arguments for launching the application are app-specific 
+    Start-Process -FilePath $using:install_app -ArgumentList "/S" -Wait
+    # Remove the share
+    Remove-PSDrive $using:drive_name -Force
+}
+
+
+# Get current logged-in user from remote computer
+function Get-CurrentUser {
+    [CmdletBinding()]
+    param( 
+        [Parameter(Mandatory = $true)]
+        [string]$ComputerName
+    )
+  
+    $queryResults = (qwinsta /server:$ComputerName | ForEach-Object { (($_.trim() -replace "\s+",","))} | ConvertFrom-Csv)
+    foreach ($i in $queryResults.username){
+        try {$i.ToUInt32($null) -ne "String" | Out-Null} 
+        catch {$result += $i}
+    }
+  return $result
+  }
+
