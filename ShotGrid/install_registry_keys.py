11d0fb3527b914cd44ac2259d2a29eb9be7d4feb
Mads Hangaard <mads.hangaard@live.dk>
2023-02-03 15:36:20 +0100

    .

--- ShotGrid/install_registry_keys.py
+++ ShotGrid/install_registry_keys.py
@@ -2,12 +2,16 @@ import os
 import shutil
 import subprocess
 
-file_name = 'ShotGrid_AMI.reg'
-folder = os.path.dirname(__file__)
-file_path = os.path.join(folder, file_name)
-new_file_path = os.path.join(f'C:\\Users\\{os.getlogin()}\\Desktop', file_name)
-shutil.copy(file_path, new_file_path)
-command = f"regedit.exe /S {new_file_path}"
-subprocess.Popen(command, shell=True)
+def install():
+    file_name = 'ShotGrid_AMI.reg'
+    folder = os.path.dirname(__file__)
+    file_path = os.path.join(folder, file_name)
+    new_file_path = os.path.join(f'C:\\Users\\{os.getlogin()}\\Desktop', file_name)
+    shutil.copy(file_path, new_file_path)
+    command = f"regedit.exe /S {new_file_path}"
+    subprocess.Popen(command, shell=True)
+    os.remove(new_file_path)
+    print('\n\n > Done\n')
 
-print('Hi :)')
+if __file__ == "__main__":
+    install()
