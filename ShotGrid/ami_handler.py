e7a544c1332bd30e46c58cd00ee5a80f461da6db
Mads Hangaard <mads.hangaard@live.dk>
2023-02-08 10:16:34 +0100

    .

--- ShotGrid/ami_handler.py
+++ ShotGrid/ami_handler.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import urllib.parse
 import pprint
 import shotgrid.wrapper as sg
