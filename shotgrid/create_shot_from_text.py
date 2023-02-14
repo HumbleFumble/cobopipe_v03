d41e7d2fcb99048055fe5976dbfdc9690ff6d11d
Mads Hangaard <mads.hangaard@live.dk>
2023-02-14 10:01:15 +0100

    .

--- shotgrid/create_shot_from_text.py
+++ shotgrid/create_shot_from_text.py
@@ -1,6 +1,6 @@
 import shotgrid.wrapper as sg
 
-def ready_up_classes(list_of_shots=None,project_name=None,seq_name=None,task_template=r"CB// Anim 3D"):
+def ready_up_classes(list_of_shots=None, project_name=None, seq_name=None, task_template=r"CB// Anim 3D"):
     list_of_wrap_shots = []
     p = sg.Project(code=project_name)
     seq = sg.Sequence(project=p.identity, code=seq_name)
@@ -15,5 +15,5 @@ def ready_up_classes(list_of_shots=None,project_name=None,seq_name=None,task_tem
 if __name__ == "__main__":
     project_name = "LegoFriends"
     seq_name = "S105_SQ010"
-    shot_list = [["S105_SQ010_SH015",20]]
+    shot_list = [["S105_SQ010_SH015", 20]]
     ready_up_classes(list_of_shots=shot_list,project_name=project_name,seq_name=seq_name)
