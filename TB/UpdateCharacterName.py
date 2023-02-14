e9136ac5b9f623b5a62f3f89bb589d4f9a941a35
Plamen Peev <freshmaker21@gmail.com>
2023-02-14 09:07:38 +0100

    Updated

--- TB/UpdateCharacterName.py
+++ TB/UpdateCharacterName.py
@@ -8,12 +8,14 @@ import TB.CB_SetupRenderInfo_Python as rs
 
 def externalRendering(scene_file):
     from ToonBoom import harmony
+
     # print("Trying to render out: %s" % scene_file)
     harmony.open_project( scene_file )                                    #Open an offline Harmony project
     session = harmony.session()                                   #Fetch the currently active session of Harmony
     project = session.project
     #The project that is already loaded.
     # print( "Current Project: %s" % (project.project_path) )
+
     '''
     shot_name = "_SH010"
     rename_node = []
@@ -27,11 +29,49 @@ def externalRendering(scene_file):
 
     '''
     rename_node = []
+    # Get all nodes
     nodes = project.scene.nodes
-    for node in nodes:
-        if "RENDER" in node.name:
-            print(node.attributes)
-            print(node.parent_group())
-    print(rename_node)
-externalRendering(r"P:\930462_HOJ_Project\Production\Film\S105\S105_SQ010\S105_SQ010_SH010\S105_SQ010_SH010\Test_H010_V100.xstage")
 
+    # Get all group nodes
+    groupNodes = project.scene.nodes
+
+    # Print all nodes with "RENDER" in the name
+
+    # for node in nodes:
+    #     if "RENDER" in node.name:
+    #         #print(node.attributes)
+    #         print(node.name)
+    # Print parent group name and node attributes
+    # for node in nodes:
+    #     if "RENDER" in node.name:
+    #
+    #         #print(node.attributes)
+    #         print(node.parent_group())
+
+    # Print all nodes that contain "RENDER" in the name
+    # for i in groupNodes:
+    #     if "RENDER" in i.name:
+    #             print(i)
+
+
+    # render_nodes = []
+    # for node in nodes:
+    #     if "RENDER" in node.name:
+    #         render_nodes.append(node)
+    #
+    # for i in render_nodes[0].attributes:
+    #     print(i)
+
+    # Iterating on the node list with a for-loop
+    render_nodes = []
+    for node in nodes:  # For loop on the node list.
+        if 'Guska' in node.path and "RENDER" in node.path:
+            render_nodes.append(node)
+
+    for node in render_nodes:
+        name = node.name
+        node.name = name.replace('Trin', 'Guska')
+
+    project.save_all()
+
+externalRendering(r"\\dumpap3\production\930462_HOJ_Project\Production\Film\S105\S105_SQ010\S105_SQ010_SH010\S105_SQ010_SH010\Test_H010_V100.xstage")
