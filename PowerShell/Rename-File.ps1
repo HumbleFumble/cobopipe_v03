51ba55566edede773d6c614ff4747bc24850efd3
n00bcybot <freshmaker21@gmail.com>
2023-02-19 18:38:25 +0100

    Commit

--- PowerShell/Rename-File.ps1
+++ PowerShell/Rename-File.ps1
@@ -7,7 +7,7 @@ function Rename-File {
         [switch]$Rename        
     )
     $items = Get-ChildItem -Path $Directory -Recurse
-    $newlist = New-Object System.Collections.ArrayList
+    $new = New-Object System.Collections.ArrayList
     ForEach ($item in $items){
         if($item.BaseName -match $MatchString){
             if ($FullPath){
@@ -15,11 +15,11 @@ function Rename-File {
                 $newlist += $item
             }else{
                 Write-Host "Match found in" $item.Directory.Parent.Name
-            $newlist += $item
+            $new += $item
             }
         }
     }
-    if(!($newlist)){
+    if(!($new)){
         Write-Host "No item matching basename `"$MatchString`" was found" -ForegroundColor Yellow
     }else {
         $index = 0
@@ -43,29 +43,3 @@ function Rename-File {
 }
 # END Rename-File
 #-------------------------------------------------------------------------------------------------------------------------------------------
-
-# ForEach ($item in $items){
-#     if($item.BaseName -match $MatchString){
-#         if ($FullPath){
-#             Write-Host "Match found in" $item.Directory.Parent.Name " - " $item.Directory
-#             $new += $item
-#         }else{
-#             Write-Host "Match found in" $item.Directory.Parent.Name
-#         $new += $item
-#         }
-#     }
-# }
-
-# Display only non-matching results
-#
-# foreach ($item in $items){
-#     if ($null -eq $newlist[$list.IndexOf($item)]){
-#         $newlist.Add($item)
-#     }else{
-#         foreach ($n in $newlist){
-#             if ($n -ne $item){
-#                 $newlist.Add($item)
-#             }
-#         }
-#     }
-# }
