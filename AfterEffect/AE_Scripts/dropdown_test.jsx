b967ff0441f55c233469f4d6a7669b7f11709c8b
HumbleFumble <christian.gyldmark@gmail.com>
2023-02-20 07:46:03 +0100

    test of ae dropdowns

---
+++ AfterEffect/AE_Scripts/dropdown_test.jsx
@@ -0,0 +1,26 @@
+﻿var cur_win = (function(thisObj){
+    var isPanel = thisObj instanceof Panel; // true or false
+    var dialog = isPanel  ? thisObj : new Window("window", "GatherHookup Direct");
+
+    dialog.alignChildren = 'left'
+    dialog.alignment = ['top','fill']
+
+    dialog.grp = dialog.add("Group{orientation:'column',alignment:['fill','fill'],\
+    panel_group: Group{orientation: 'row',\
+    static_group: Group{orientation: 'column',\
+    drop_down: DropDownList { alignment:['fill','top'],preferredSize: ['100','20'] }},\
+    edit_group: Group{orientation: 'column',et_e: EditText{text: '01', preferredSize: ['50','20']}, et_seq: EditText{text: '020', preferredSize: ['50','20']} } }}\
+    ")
+
+    dialog.grp.panel_group.static_group.drop_down.add("item", "new");
+    dialog.grp.panel_group.static_group.drop_down.selection="new"
+    if (!isPanel) {
+      // if it's a window
+      dialog.show();
+    } else {
+      // if it's a panel
+      dialog.layout.layout(true);
+      dialog.layout.resize();
+      }
+    return dialog
+})(this);
