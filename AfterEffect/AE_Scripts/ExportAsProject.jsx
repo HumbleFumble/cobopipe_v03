bd3983d8da87a5423f7d3ce9e9ef5fcc0957497d
HumbleFumble <christian.gyldmark@gmail.com>
2023-02-22 10:34:33 +0100

    fixed path

--- AfterEffect/AE_Scripts/ExportAsProject.jsx
+++ AfterEffect/AE_Scripts/ExportAsProject.jsx
@@ -4,8 +4,8 @@
 
 cc = getConfig()
 var pipepath = cc.project_paths["python_path"]
-var pipepath = "C:/Users/cg/PycharmProjects/cobopipe_v02-001/AfterEffect/"
-//var python_script_path = pipepath + "/ExportAsProject_Python.py";
+//var pipepath = "C:/Users/cg/PycharmProjects/cobopipe_v02-001/AfterEffect/"
+var python_script_path = pipepath + "/ExportAsProject_Python.py";
 
 function Export(orig_file_path,export_path){
     log(orig_file_path)
