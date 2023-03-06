0809d99e0b5fc679a34450d723d172de0c22d558
HumbleFumble <christian.gyldmark@gmail.com>
2023-03-06 14:15:35 +0100

    made script for copying WIP bgs to Anim

---
+++ Photoshop/CreateTempDuplicate.jsx
@@ -0,0 +1,7 @@
+﻿function Run(){
+    var curDoc = app.activeDocument;
+    var temp_name = (curDoc.name).split(".")
+    var doc_duplicate = curDoc.duplicate( temp_name[0] + "_Temp." + temp_name[1]);
+    var curDoc = app.activeDocument;
+    }
+Run()
