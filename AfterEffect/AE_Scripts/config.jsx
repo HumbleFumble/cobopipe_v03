b2f21647a4c0bb1f11c6701223f246bc6b6bd27e
Mads Hangaard <mads.hangaard@live.dk>
2023-03-28 16:03:22 +0200

    get_config() should now work as intended for After Effects

--- AfterEffect/AE_Scripts/config.jsx
+++ AfterEffect/AE_Scripts/config.jsx
@@ -1,7 +1,7 @@
  #include "json2.js";
 
-function config_ui(){
-	var ui_script = "window { \
+function config_prompt(){
+	var ui_script = "dialog { \
         text: 'Select Project', \
         alignChildren: 'center', \
         alignment: ['top','fill'], \
@@ -16,45 +16,58 @@ function config_ui(){
                 alignment: 'left' \
                 }, \
             apply_button: Button{ \
-                preferredSize: [335, 30], \
+                preferredSize: [75, 30], \
+                alignment: 'right', \
                 text: 'Apply' \
                 }, \
         } \
     }";
 		
 	var ui = new Window(ui_script);
-	// my_window.queue_panel.buttonQ_group.runQ_button.onClick = Run;
     populate_dropdown(ui.main_group.dropdown_list, true)
+    ui.main_group.dropdown_list.selection = 0;
     ui.main_group.checkbox.value = true;
-    ui.main_group.apply_button.onClick = function() {
-        var project = ui.main_group.dropdown_list.selected
-        alert(project)
-        ui.close()
+    ui.main_group.checkbox.onClick = function() {
+        populate_dropdown(ui.main_group.dropdown_list, ui.main_group.checkbox.value);
+        ui.main_group.dropdown_list.selection = 0;
     }
-
-	return ui;
+    ui.main_group.apply_button.onClick = function() {
+        var project = ui.main_group.dropdown_list.selection;
+        var project_list = get_config_options(false);
+        for(var i in project_list){
+            if(project.toString() === project_list[i]){
+                ui.close(i);
+            }
+        }
+    }
+    return ui;
 }
 
 function populate_dropdown(dropdown, hide_archived){
+    // var selection = dropdown
     dropdown.removeAll();
     var projects = get_config_options(hide_archived);
     for(var i in projects){
-        dropdown.add("item", projects[i])
+        dropdown.add("item", projects[i]);
     }
 }
 
 function get_config(){
-	var project_name = $.getenv( "BOM_PROJECT_NAME" )
-	if(project_name == null){
-		var config_folder = Folder('T:/_Pipeline/cobopipe_v02-001/Configs');
-		var files = config_folder.getFiles();
-		var config_files = [];
-		for(i in files){
-			if(files[i].fsName.substr(-5, 5) == '.json'){
-				config_files.push(files[i])
-			}
-		}
-	}
+	var project = get_project();
+    var config_path = 'T:/_Pipeline/cobopipe_v02-001/Configs/Config_' + project + '.json';
+    return config;
+}
+
+function get_project(){
+    var project = $.getenv( "BOM_PROJECT_NAME" );
+	if(project == null){
+		var ui = config_prompt();
+        var index = ui.show();
+        var project_list = get_config_options(false);
+        return project_list[index];
+	} else {
+        return project;
+    }
 }
 
 function get_config_options(hide_archived){
@@ -64,31 +77,28 @@ function get_config_options(hide_archived){
 
     for(var i in files){
         if(files[i].fsName.substr(-5, 5) == '.json'){
-            filename = files[i].fsName.split('\\')[files[i].fsName.split('\\').length-1]
+            filename = files[i].fsName.split('\\')[files[i].fsName.split('\\').length-1];
             if(filename.substr(0, 7) == 'Config_'){
-                filename = filename.replace('Config_', '').replace('.json', '')
-                projects.push(filename)
+                filename = filename.replace('Config_', '').replace('.json', '');
+                projects.push(filename);
             }
         }
     }
     
-    var archived_projects_object = loadJson('T:/_Pipeline/cobopipe_v02-001/Configs/archivesProjects.json');
-    var archived_projects = [];
-    for(var i in archived_projects_object){
-        archived_projects.push(archived_projects_object[i])
-    }
+    var archived_projects = loadJson('T:/_Pipeline/cobopipe_v02-001/Configs/archivesProjects.json');
+
     if(hide_archived == true){
-        var matches = [];
+        var non_archived_projects = [];
         for(var i in projects){
+            var match = false;
             for(var j in archived_projects){
                 if(projects[i] === archived_projects[j]){
-                    matches.push(projects[i])
+                    match = true;
                 }
             }
-        }
-        for(var i in matches){
-            alert('Hi')
-            alert(projects.indexOf(matches[i]))
+            if(match == false){
+                    non_archived_projects.push(projects[i]);
+            }
         }
         return non_archived_projects;
     } else {
@@ -101,10 +111,5 @@ function loadJson(path){
     scriptFile.open('r');
     var data = scriptFile.read();
     scriptFile.close();
-    // return data;
     return JSON.parse(data);
 }
-
-var ui = config_ui();
-ui.show();
-// get_config_options();
