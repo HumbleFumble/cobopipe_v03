e56dea9d72275242ebbbb6baaf586398764dee27
Mads Hangaard <mads.hangaard@live.dk>
2023-03-28 13:11:53 +0200

    .

--- BAT_files/runDeleteThinkBox.bat
+++ BAT_files/runDeleteThinkBox.bat
@@ -1,2 +1,2 @@
-pwsh -Command "Set-ExecutionPolicy Bypass;& T:/_Pipeline/cobopipe_v02-001/PowerShell/ScriptBlocks/DeleteThinkBoxFolder.ps1"
+pwsh -Command "Set-ExecutionPolicy Bypass;& \\192.168.0.225/tools/_Pipeline/cobopipe_v02-001/PowerShell/ScriptBlocks/DeleteThinkBoxFolder.ps1"
 PAUSE
