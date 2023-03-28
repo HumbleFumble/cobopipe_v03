5edcdca279af24bf0bc948d5b14559e32ed54d78
Plamen Peev <freshmaker21@gmail.com>
2023-03-28 11:41:47 +0200

    Latest

--- PowerShell/ScriptBlocks/DeleteThinkBoxFolder.ps1
+++ PowerShell/ScriptBlocks/DeleteThinkBoxFolder.ps1
@@ -2,9 +2,12 @@
 param (
     [Parameter(Position = 0)][string[]]$ComputerName
 )
+Set-ExecutionPolicy Bypass
 
 $creds = Get-Credential -UserName cphbom\deadline -Message:$false
 Invoke-Command -ComputerName $ComputerName -ScriptBlock { 
     $path = "C:\Users\deadline\AppData\Local\Thinkbox"
     Remove-Item -Path $path -Confirm:$false -Recurse
 } -Credential $creds
+
+Set-ExecutionPolicy Restricted
