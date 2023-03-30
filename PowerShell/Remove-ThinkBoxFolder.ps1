c1bfc2ee69efb2ae83861e4ada6275cd24a11f08
HumbleFumble <christian.gyldmark@gmail.com>
2023-03-30 11:30:48 +0200

    hardcoded a powershell local version for removing deadline folder

--- PowerShell/Remove-ThinkBoxFolder.ps1
+++ PowerShell/Remove-ThinkBoxFolder.ps1
@@ -2,9 +2,11 @@ function Remove-ThinkBoxFolder {
     param (
         [Parameter()][string[]]$ComputerName = "computername"
     )
+    Set-ExecutionPolicy Bypass
     $creds = Get-Credential -UserName cphbom\deadline -Message:$false
     Invoke-Command -ComputerName $ComputerName -ScriptBlock { 
         $path = "C:\Users\deadline\AppData\Local\Thinkbox"
         Remove-Item -Path $path -Confirm:$false -Recurse
     } -Credential $creds
+    Set-ExecutionPolicy Restricted
 }
