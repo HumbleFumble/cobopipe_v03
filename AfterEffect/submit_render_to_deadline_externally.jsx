40432ea72d78ad109b255e195aae7cf9deed5470
Mads Hangaard <mads.hangaard@live.dk>
2023-04-11 11:50:26 +0200

    Maybe fix?

--- AfterEffect/submit_render_to_deadline_externally.jsx
+++ AfterEffect/submit_render_to_deadline_externally.jsx
@@ -1,5 +1,5 @@
 #target.aftereffects;
-#include "AE_Scripts/submit_render_to_deadline.jsx"
+#include "AE_Scripts/includes/deadline_submit.jsx"
 
 function Main(file_path){
 	app.open(new File(file_path));
