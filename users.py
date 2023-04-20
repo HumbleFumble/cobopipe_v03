8cff3019d1d74f4c09f030dc8b3cb4c49191328a
Mads Hangaard <mads.hangaard@live.dk>
2023-04-20 16:28:59 +0200

    .

--- users.py
+++ users.py
@@ -3,6 +3,12 @@ from Maya_Functions.file_util_functions import saveJson, loadJson
 from PySide2 import QtWidgets, QtCore, QtGui
 from getConfig import getConfigClass
 
+try:
+	import maya.cmds as cmds
+	in_maya = True
+except:
+	in_maya = False
+
 CC = getConfigClass()
 
 
@@ -17,6 +23,7 @@ class add_user_ui(QtWidgets.QDialog):
         flags = self.windowFlags() & ~QtCore.Qt.WindowContextHelpButtonHint
         flags = flags | QtCore.Qt.WindowStaysOnTopHint
         self.setWindowFlags(flags)
+        self.add_user_ui_palette()
 
         self.main_layout = QtWidgets.QHBoxLayout()
         self.input = QtWidgets.QLineEdit()
@@ -33,6 +40,29 @@ class add_user_ui(QtWidgets.QDialog):
     def add(self):
         self.signals.add.emit(self.input.text())
         self.close()
+
+    def add_user_ui_palette(self):
+		#TODO setting palette in maya does NOT look great
+        if not in_maya:
+            magenta = {"R": 142, "G": 45, "B": 197}
+            blue = {"R": 50, "G": 50, "B": 255}
+
+            hi_lgt = blue
+            palette = QtGui.QPalette()
+            palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
+            palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
+            palette.setColor(QtGui.QPalette.Base, QtGui.QColor(40, 40, 40))
+            palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
+            palette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
+            palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
+            palette.setColor(QtGui.QPalette.Text, QtCore.Qt.lightGray)
+            palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
+            palette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.white)
+            palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
+            palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(hi_lgt["R"], hi_lgt["G"], hi_lgt["B"]).lighter(100))
+            palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.white)
+            self.setPalette(palette)
+            QtWidgets.QApplication.instance().setStyle('Fusion')
 
 
 def get_users(key=None):
