cb0459110e12051ea5b50c869dd9138e7a8fb213
HumbleFumble <christian.gyldmark@gmail.com>
2023-06-06 07:47:30 +0200

    new quick script

--- Fusion_Functions/Calls/__FindLoadersCall.py
+++ Fusion_Functions/Calls/__FindLoadersCall.py
@@ -1,6 +1,6 @@
 import site
-site.addsitedir("T:/_Pipeline/cobopipe_v02-001/")
-#site.addsitedir("C:/Users/cg/PycharmProjects/cobopipe_v02-001/")
+#site.addsitedir("T:/_Pipeline/cobopipe_v02-001/")
+site.addsitedir("C:/Users/cg/PycharmProjects/cobopipe_v02-001/")
 
 # Simply import file from directory to execute
 import Fusion_Functions.Fusion_Utility as FU
