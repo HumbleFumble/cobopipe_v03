1d79387641b01d9da85b75573aa0399a1df61d05
HumbleFumble <christian.gyldmark@gmail.com>
2023-06-19 11:30:02 +0200

    done with loader script for now :)

--- Fusion_Functions/changeLoaders.py
+++ Fusion_Functions/changeLoaders.py
@@ -16,10 +16,27 @@ class ChangeLoaders(QtWidgets.QDialog):
             self.loader_dict = {}
         else:
             self.__fusion = None
+        self.setWindowTitle("Change Loaders")
+        self.loaderFrame_dict = {}
+        self.ignore_list = []
         self.sortAllLoaders()
+        comp_data = self.comp.GetData("loader_ignore_list")
+        if comp_data:
+            self.ignore_list = list(comp_data.values())
+            self.updateSceneData()
         self.createWindow()
+        self.updateVisibility()
+
+
         # self.setMaximumHeight(400)
         # self.resize(400,400)
+    def updateSceneData(self):
+        scene_loaders = set(self.loader_dict.keys())
+
+        new_ignore = set(self.ignore_list).intersection(scene_loaders)
+
+        self.ignore_list = list(new_ignore)
+        self.comp.SetData("loader_ignore_list",self.ignore_list)
 
     def sortAllLoaders(self):
         all_loaders = self.comp.GetToolList(False, "Loader").values()
@@ -48,34 +65,28 @@ class ChangeLoaders(QtWidgets.QDialog):
 
     def createWindow(self):
 
-        # self._scrollAreaWidget = QtWidgets.QScrollArea()
-        # self.setLayout(self._scrollAreaWidget)
-
-        # Add tabs
-        # self._tabWidget = QtWidgets.QTabWidget()
-        # self._tabWidget.addTab(self.createTabOneLayout(), 'Test 1')
-        # self._tabWidget.addTab(self.createTabOneLayout(), 'Test 2')
-        # self._tabWidget.setTabPosition(QtWidgets.QTabWidget.East)
-
         self.scroll_lay = QtWidgets.QVBoxLayout()
         self.scrollarea = QtWidgets.QScrollArea()
+        # self.scrollarea.setMinimumWidth(500)
 
-
-        #
-        # self.scroll_widget = QtWidgets.QWidget()
 
         self.layout_widget = QtWidgets.QWidget()
         self.layout_top = QtWidgets.QVBoxLayout()
-
+        self.layout_top.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
         self.checkbox_layout = QtWidgets.QHBoxLayout()
-        self.checkbox = QtWidgets.QCheckBox("Hide ignored loaders")
-        self.checkbox.setChecked(True)
-
-        self.checkbox_layout.addWidget(self.checkbox)
+        self.ignore_list_checkbox = QtWidgets.QCheckBox("Hide ignored loaders")
+        self.ignore_list_checkbox.setChecked(True)
+        self.ignore_list_count = QtWidgets.QLabel(f"Hidden Loaders: {len(self.ignore_list)}")
+        self.ignore_list_checkbox.stateChanged.connect(self.updateVisibility)
+        self.checkbox_layout.addWidget(self.ignore_list_checkbox)
+        self.checkbox_layout.addWidget(self.ignore_list_count)
 
         self.layout_top.addLayout(self.checkbox_layout)
-        for k,l in self.loader_dict.items():
-            self.layout_top.addWidget(loaderFrame(k,l,self))
+        for k in sorted(self.loader_dict.keys()):
+            l = self.loader_dict[k]
+            lf = loaderFrame(k,l,self)
+            self.layout_top.addWidget(lf)
+            self.loaderFrame_dict[k] = lf
 
         self.layout_widget.setLayout(self.layout_top)
 
@@ -91,10 +102,12 @@ class ChangeLoaders(QtWidgets.QDialog):
         self.scrollarea.setWidget(self.layout_widget)
         self.scroll_lay.addWidget(self.scrollarea)
         self.scrollarea.setWidgetResizable(True)
+        # self.scrollarea.setMinimumWidth(self.layout_widget.sizeHint().width())
+
         self.setLayout(self.scroll_lay)
 
     def clickBrowse(self,cur_loaderFrame):
-        current_text = cur_loaderFrame.loader_key.text()
+        current_text = cur_loaderFrame.loader_key
         comp_path = ""
 
         if self.__fusion:
@@ -106,43 +119,96 @@ class ChangeLoaders(QtWidgets.QDialog):
         fileName = QtWidgets.QFileDialog.getOpenFileName(self,"Pick Input - First frame if stack", comp_path, "Image Files (*.png *.jpg *.bmp *.exr *.mov *.mp4)")[0]
 
         if fileName:
-            cur_loaderFrame.path_text.setText(fileName)
+            # cur_loaderFrame.path_text.setText(fileName)
             self.changeClip(loader_key=cur_loaderFrame.loader_key,new_path=fileName)
             cur_loaderFrame.loader_key = fileName
+            cur_loaderFrame.setTextAndTooltip(fileName)
+            if current_text in self.ignore_list:
+                self.ignore_list.remove(current_text)
+                if cur_loaderFrame.ignore_checkbox.isChecked:
+                    self.updateIgnoreList(cur_loaderFrame)
 
-            # cur_loaderFrame.
+    def updateIgnoreList(self,cur_loaderFrame,*args):
 
+        if cur_loaderFrame.ignore_checkbox.isChecked():
+            if not cur_loaderFrame.loader_key in self.ignore_list:
+                self.ignore_list.append(cur_loaderFrame.loader_key)
+        else:
+            if cur_loaderFrame.loader_key in self.ignore_list:
+                self.ignore_list.remove(cur_loaderFrame.loader_key)
+        self.comp.SetData("loader_ignore_list",self.ignore_list)
+        self.ignore_list_count.setText(f"Hidden Loaders: {len(self.ignore_list)}")
+        self.updateVisibility()
+    def updateVisibility(self,*args):
+        for i in self.ignore_list:
+            if self.ignore_list_checkbox.isChecked():
+                # print(self.loaderFrame_dict[i].loader_key)
+                self.loaderFrame_dict[i].setVisible(False)
+            else:
+                self.loaderFrame_dict[i].setVisible(True)
+    def pickLoader(self,cur_loaderFrame):
+        current_active = self.comp.ActiveTool()
+        final_number = 0
+        for number,l in enumerate(cur_loaderFrame.loader_list):
+            if str(l) == str(current_active):
+                if number == (len(cur_loaderFrame.loader_list)-1):
+                    final_number = 0
+                else:
+                    final_number = number + 1
+        self.comp.SetActiveTool(cur_loaderFrame.loader_list[final_number])
 
 class loaderFrame(QtWidgets.QFrame):
     def __init__(self, loader_key, loader_list,parent):
         super(loaderFrame, self).__init__()
         # self.setStyleSheet("border: 1px solid black; margin: 0px; padding: 0px;")
+        self.parent_class = parent
         self.loader_key = loader_key
         self.loader_list = loader_list
         self.layout = QtWidgets.QHBoxLayout()
+
         # self.layout.setContentsMargins(0, 0, 0, 0)
-        # try:
-        #     current_text = "/".join(self.loader_key.split("/")[-3:0])
-        # except:
-        #     current_text = self.loader_key
-        self.path_text = QtWidgets.QLineEdit(self.loader_key)
-        self.path_text.setMinimumWidth(250)
+
+        # current_text = os.path.split(self.loader_key)
+        # current_dir = os.path.dirname(self.loader_key)
+
+        self.path_text = QtWidgets.QLineEdit()
+        self.path_text.setMinimumWidth(450)
+
+        self.setTextAndTooltip(path=self.loader_key)
 
         self.browse_button = QtWidgets.QPushButton("Browse")
 
-        self.browse_button.clicked.connect(partial(parent.clickBrowse,self))
+        self.browse_button.clicked.connect(partial(self.parent_class.clickBrowse,self))
+        
+        self.pick_button = QtWidgets.QPushButton("Pick")
+        self.pick_button.clicked.connect(partial(self.parent_class.pickLoader,self))
 
+        self.number_of_loaders = QtWidgets.QLabel(f"Amount: {len(self.loader_list):02}")
+        # self.number_of_loaders.setMinimumWidth(60)
         self.ignore_checkbox = QtWidgets.QCheckBox("Ignore")
+        if self.loader_key in self.parent_class.ignore_list:
+            self.ignore_checkbox.setChecked(True)
+        self.ignore_checkbox.stateChanged.connect(partial(self.parent_class.updateIgnoreList, self))
+        self.layout.addWidget(self.ignore_checkbox)
         self.layout.addWidget(self.path_text)
         self.layout.addWidget(self.browse_button)
-        self.layout.addWidget(self.ignore_checkbox)
+        self.layout.addWidget(self.pick_button)
+        self.layout.addWidget(self.number_of_loaders)
+
+        # self.setMinimumWidth(self.path_text.sizeHint().width())
         self.setLayout(self.layout)
 
-    # def return_line(self):
-    #     return self.lineEdit
-    #
-    # def return_browse(self):
-    #     return self.browse_button
+    def setTextAndTooltip(self, path):
+        # try:
+        path_split = path.split("/")
+        if len(path_split)>2:
+
+            current_text = "/".join(path_split[-2:])
+        else:
+            current_text = path_split[-1]
+
+        self.path_text.setText(current_text)
+        self.path_text.setToolTip(self.loader_key)
 
 
 def run(fusion=None):
@@ -152,7 +218,8 @@ def run(fusion=None):
         app = QtWidgets.QApplication.instance()
     win = ChangeLoaders(fusion=fusion)
     # win.setMaximumHeight(400)
-    win.resize(400, 100)
+    # win.resize(700, 400)
+
     # win.show()
     if win.exec_():
         value = win.getValue()
