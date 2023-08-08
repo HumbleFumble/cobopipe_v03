3ffacbe207ca61acc8d737bef9eaf644d0406794
HumbleFumble <christian.gyldmark@gmail.com>
2023-08-08 11:13:49 +0200

    tweaking harmony script calls to work on local machines automatically.

--- TB/ToonBoom_Global_Scripts/CB_PreviewPython_Call.js
+++ TB/ToonBoom_Global_Scripts/CB_PreviewPython_Call.js
@@ -1,25 +1,25 @@
 function PreviewPythonRun(){
     var script_name = "CB_SetLineThickness.py";
 
-    var local_folder = specialFolders.userScripts
-    var script_path = local_folder + "/" + script_name
-    MessageLog.trace("Checking local path: " + script_path)
-    var file = new File( script_path );
-    if ( file.exists ){
-        MessageLog.trace("Found script at local path. Running now: " + script_name)
-        var myPythonObject = PythonManager.createPyObject(script_path);
+    var server_script_path = System.getenv("BOM_PIPE_PATH")
+
+    if(server_script_path){
+        MessageLog.trace("Found script at global path. Running now: " + script_name)
+        var myPythonObject = PythonManager.createPyObject(server_script_path +"/TB/CB_PreviewPython.py");
     }else{
-        MessageLog.trace("No local script. Looking for a global script location")
-        var server_script_path = System.getenv("BOM_PIPE_PATH")
-        if(server_script_path){
-            MessageLog.trace("Found script at global path. Running now: " + script_name)
-            var myPythonObject = PythonManager.createPyObject(server_script_path +"/TB/CB_PreviewPython.py");
-        }else{
-            MessageLog.trace("Can't find python file to load. Stopping Execution of: " + script_name)
-            return 0
+        var local_folder = specialFolders.userScripts
+        var script_path = local_folder + "/" + script_name
+        MessageLog.trace("Checking local path: " + script_path)
+        var file = new File( script_path );
+        if ( file.exists ){
+            MessageLog.trace("Found script at local path. Running now: " + script_name)
+            var myPythonObject = PythonManager.createPyObject(script_path);
+            }else{
+                MessageLog.trace("Can't find python file to load. Stopping Execution of: " + script_name)
+                return 0
+            }
         }
     }
-
     myPythonObject.setObject("js_exporter",exporter);
     myPythonObject.py.run();
 }
