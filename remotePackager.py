749c9166376a32c404a335b3d455064079e83c22
HumbleFumble <christian.gyldmark@gmail.com>
2023-08-08 14:43:11 +0200

    starting a remotepackage builder for putting together zips of what we need to setup remote workers.

--- remotePackager.py
+++ remotePackager.py
@@ -2,12 +2,16 @@ import os
 import zipUtil
 
 package_dict = {
-    "ffmpeg":["T:/_Executables/ffmpeg"],
+    "ffmpeg":["T:/_Executables/ffmpeg/"],
     "python39":["T:/_Software/Python/python-3.9.1-amd64.exe"],
+    "python_requirements":["T:/_Pipeline/cobopipe_v02-001/requirements.txt"],
     "harmony22": ["T:/_Software/Toonboom/HAR22-PRM-win-21617.exe"],
-    "harmony-scripts":["T:\_Pipeline\cobopipe_v02-001\TB\Scripting_Hotbars\Toon Boom Harmony Premium\2100-scripts"]
-
-
+    "harmony-scripts":["T:/_Pipeline/cobopipe_v02-001/TB/Scripting_Hotbars/Toon Boom Harmony Premium/2100-scripts/",
+                       "T:/_Pipeline/cobopipe_v02-001/TB/ToonBoom_Global_Python/",
+                       "T:/_Pipeline/cobopipe_v02-001/Preview/ffmpeg_util.py",
+                       "T:/_Pipeline/cobopipe_v02-001/icon/"],
+    "mountDriveInterface":["T:/_Pipeline/cobopipe_v02-001/MountDriveInterface.py"],
+    "shotBrowser_bat":["T:/_Pipeline/cobopipe_v02-001/BAT_files/ShotBrowser.bat"]
 }
 
 
