f964513a1f85d5eb49c6ee28e50d61ad5ee37c26
HumbleFumble <christian.gyldmark@gmail.com>
2023-08-31 11:56:39 +0200

    figured out how to run powershell with params. Just don't put it inside a function...

--- PowerShell/Edit-EnvironmentVariable.ps1
+++ PowerShell/Edit-EnvironmentVariable.ps1
@@ -1,59 +1,57 @@
-function Edit-EnvironmentVariable {
-    param(
-    [Parameter(Mandatory=$true)][string]$Variable,
-    [String]$VariableKey="Path",
-    [Parameter()][Boolean]$ReplaceKey=$false,
-    [Boolean]$RemoveVarible=$false
-    )
-    Write-Host "Env Var: " $VariableKey
-    Write-Host "Removing: " $RemoveVarible
-    if (-Not $RemoveVarible){
-         $envmachine = [Environment]::GetEnvironmentVariable($VariableKey, [EnvironmentVariableTarget]::Machine)
-         Write-Host "HERE is current env: " $envmachine
-         if($envmachine)
 
+param(
+[Parameter(Mandatory=$true)][string]$Variable,
+[String]$VariableKey="Path",
+[Parameter()][Boolean]$ReplaceKey=$false,
+[Boolean]$RemoveVarible=$false
+)
+Write-Host "Env Var: " $VariableKey
+Write-Host "Removing: " $RemoveVarible
+if (-Not $RemoveVarible){
+     $envmachine = [Environment]::GetEnvironmentVariable($VariableKey, [EnvironmentVariableTarget]::Machine)
+     Write-Host "HERE is current env: " $envmachine
+     if($envmachine)
+
+     {
+         if ($true -eq $ReplaceKey)
          {
-             if ($true -eq $ReplaceKey)
+             [Environment]::SetEnvironmentVariable($VariableKey, $Variable, [EnvironmentVariableTarget]::Machine)
+         }
+         else
+         {
+             if (-Not $envmachine.Contains($Variable))
              {
-                 [Environment]::SetEnvironmentVariable($VariableKey, $Variable, [EnvironmentVariableTarget]::Machine)
+                 Write-Host "Adding: " $envmachine
+                 [Environment]::SetEnvironmentVariable($VariableKey, $( ($Variable + ";") + $envmachine ), [EnvironmentVariableTarget]::Machine)
              }
-             else
-             {
-                 if (-Not $envmachine.Contains($Variable))
-                 {
-                     Write-Host "Adding: " $envmachine
-                     [Environment]::SetEnvironmentVariable($VariableKey, $( ($Variable + ";") + $envmachine ), [EnvironmentVariableTarget]::Machine)
-                 }
 
-             }
-         }else{
-                 Write-Host "not found the env" $envmachine
-                 [Environment]::SetEnvironmentVariable($VariableKey, $Variable, [EnvironmentVariableTarget]::Machine)
-             }
-    }
-    else{
-        $envmachine = [Environment]::GetEnvironmentVariable($VariableKey, [EnvironmentVariableTarget]::Machine)
+         }
+     }else{
+             Write-Host "not found the env" $envmachine
+             [Environment]::SetEnvironmentVariable($VariableKey, $Variable, [EnvironmentVariableTarget]::Machine)
+         }
+}
+else{
+    $envmachine = [Environment]::GetEnvironmentVariable($VariableKey, [EnvironmentVariableTarget]::Machine)
 
-        if($envmachine)
+    if($envmachine)
+    {
+        if ( -Not $envmachine.Contains(";"))
         {
-            if ( -Not $envmachine.Contains(";"))
+            [Environment]::SetEnvironmentVariable($VariableKey,$null, [EnvironmentVariableTarget]::Machine)
+        }else{
+            if ( ($envmachine -split ";").Contains($Variable))
             {
-                [Environment]::SetEnvironmentVariable($VariableKey,$null, [EnvironmentVariableTarget]::Machine)
-            }else{
-                if ( ($envmachine -split ";").Contains($Variable))
-                {
-                    Write-Host "removing part of : " $envmachine
-                    [Environment]::SetEnvironmentVariable($VariableKey, $($envmachine.Replace($Variable+";", "") ), [EnvironmentVariableTarget]::Machine)
-                }
-                elseif (($envmachine -split ";").Contains($Variable + "\"))
-                {
-                    Write-Host "removing part of : " $envmachine
-                    [Environment]::SetEnvironmentVariable($VariableKey, $($envmachine.Replace($Variable, "") ), [EnvironmentVariableTarget]::Machine)
-                }
-
+                Write-Host "removing part of : " $envmachine
+                [Environment]::SetEnvironmentVariable($VariableKey, $($envmachine.Replace($Variable+";", "") ), [EnvironmentVariableTarget]::Machine)
             }
+            elseif (($envmachine -split ";").Contains($Variable + "\"))
+            {
+                Write-Host "removing part of : " $envmachine
+                [Environment]::SetEnvironmentVariable($VariableKey, $($envmachine.Replace($Variable, "") ), [EnvironmentVariableTarget]::Machine)
+            }
+
         }
     }
-
 }
-Edit-EnvironmentVariable -Variable "NewEnvValue" -VariableKey "NewEnvKey"
+
