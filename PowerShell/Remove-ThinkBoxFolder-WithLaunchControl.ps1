96323bb47bae3a72488ff6a4fb0daa4e59aad3db
HumbleFumble <christian.gyldmark@gmail.com>
2023-10-04 13:48:29 +0200

    fixed a hotbar issue

---
+++ PowerShell/Remove-ThinkBoxFolder-WithLaunchControl.ps1
@@ -0,0 +1,13 @@
+
+function Remove-ThinkBoxFolder-WithLaunchControl {
+    param (
+        [Parameter()][string[]]$ComputerName = "computername"
+    )
+    & deadlinecommand.exe -RemoteControl $ComputerName StopSlave
+    $creds = Get-Credential -UserName cphbom\deadline -Message:$false
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $path = "C:\Users\deadline\AppData\Local\Thinkbox"
+        Remove-Item -Path $path -Confirm:$false -Recurse
+    } -Credential $creds
+     & deadlinecommand.exe -RemoteControl $ComputerName LaunchSlave
+}
