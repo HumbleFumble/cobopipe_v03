8382d879633cb234832c80d69d61c389144ad9c2
aficgy <christian.gyldmark@gmail.com>
2023-12-15 12:40:31 +0100

    added new script for getting workstations on domain

--- PowerShell/Get-ComputerListAndInfo.ps1
+++ PowerShell/Get-ComputerListAndInfo.ps1
@@ -1,33 +1,34 @@
-"""
+<#
 This script is meant to run through and find all the computers and get the info on them saved out.1GB
 Requires:
 Windows 10: Go to Settings → Apps → Optional features → Add a feature. Find 'RSAT: Active Directory Domain Services and Lightweight Directory Tools' and install it.
 Then in powershell (admin) run: Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
-
-
-"""
-
+#>
 
 $computers = Get-ADComputer -Filter * | Sort-Object -Property Name | Select-Object -ExpandProperty Name
-
 $results = @()
 
 foreach ($computer in $computers) {
-    try {
-        $cpu = Get-WmiObject -Class Win32_Processor -ComputerName $computer | Select-Object -ExpandProperty Name
-        $ram = Get-WmiObject -Class Win32_PhysicalMemory -ComputerName $computer | Measure-Object -Property Capacity -Sum | % { $_.Sum / 1GB }
-        $gpu = Get-WmiObject -Class Win32_VideoController -ComputerName $computer | Select-Object -ExpandProperty Name
-        $ip = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer | Where-Object { $_.IPAddress -ne $null }).IPAddress
+    # Check if the computer is online
+    if (Test-Connection -ComputerName $computer -Count 1 -Quiet) {
+        try {
+            $cpu = Get-WmiObject -Class Win32_Processor -ComputerName $computer | Select-Object -ExpandProperty Name
+            $ram = Get-WmiObject -Class Win32_PhysicalMemory -ComputerName $computer | Measure-Object -Property Capacity -Sum | % { $_.Sum / 1GB }
+            $gpu = Get-WmiObject -Class Win32_VideoController -ComputerName $computer | Select-Object -ExpandProperty Name
+            $ip = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $computer | Where-Object { $_.IPAddress -ne $null }).IPAddress
 
-        $results += [PSCustomObject]@{
-            ComputerName = $computer
-            CPU = $cpu
-            RAM = "${ram} GB"
-            GPU = $gpu
-            IP = $ip -join ', '
+            $results += [PSCustomObject]@{
+                ComputerName = $computer
+                CPU = $cpu
+                RAM = "${ram} GB"
+                GPU = $gpu
+                IP = $ip -join ', '
+            }
+        } catch {
+            Write-Warning "Failed to get info for $computer"
         }
-    } catch {
-        Write-Warning "Failed to get info for $computer"
+    } else {
+        Write-Warning "$computer is not reachable"
     }
 }
 
@@ -39,3 +40,4 @@ $filePath = "C:\Temp\Workstation_List.csv"  # Specify your desired file path
 $results | Export-Csv -Path $filePath -NoTypeInformation
 
 Write-Host "Results saved to $filePath"
+# Print to screen
