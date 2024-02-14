f31e4b1ef94a8fb3577e1775f92d2c215d631f5e
aficgy <christian.gyldmark@gmail.com>
2024-02-14 12:39:20 +0100

    working on making powershell 7.4 working. should work mostly now, on machines with remote-desktop and Enable-PSRemoting enabled

--- PowerShell/Get-ComputerListAndInfo_7-4.ps1
+++ PowerShell/Get-ComputerListAndInfo_7-4.ps1
@@ -12,10 +12,10 @@ $admin_user = "CPHAFID1\afirender"
 $PASSWORD     = ConvertTo-SecureString "royalrender" -AsPlainText -Force
 $UNPASSWORD   = New-Object System.Management.Automation.PsCredential $admin_user, $PASSWORD
 
-$computer = "CPHAFID1-RN501"
+$computer = "CPHAFID1-WS116"
 $CimSession = New-CimSession -ComputerName $computer -Credential $UNPASSWORD
-$computer_proces =  Get-CimInstance Win32_Processor -CimSession $CimSession
-
+#$computer_proces =  Get-CimInstance Win32_Processor -CimSession $CimSession
+$computer_proces = Get-CimInstance -Class Win32_PhysicalMemory -CimSession $CimSession | Measure-Object -Property Capacity -Sum | % { $_.Sum / 1GB }
 $computer_proces
 
 
@@ -27,7 +27,7 @@ $computer_proces
 
 $results = @()
 $Date = [DateTime]::Today.AddDays(-200)
-$computers = Get-ADComputer -Filter 'Name -like "*CPHAFID1-RN50*" -and LastLogonDate -ge $Date' | Sort-Object -Property Name | Select-Object -ExpandProperty Name
+$computers = Get-ADComputer -Filter 'Name -like "*CPHAFID1-WS116*" -and LastLogonDate -ge $Date' | Sort-Object -Property Name | Select-Object -ExpandProperty Name
 
 $admin_user = "CPHAFID1\afirender"
 $PASSWORD     = ConvertTo-SecureString "royalrender" -AsPlainText -Force
@@ -39,7 +39,7 @@ foreach ($computer in $computers) {
     $last_logon = Get-ADComputer -Filter 'Name -like $computer' -Properties LastLogonDate
     #if (Test-Connection -TargetName $computer -Count 1 -Quiet) {
     try {
-        $CimSession = New-CimSession -ComputerName $computer -Credential $UNPASSWORD
+        $CimSession = New-CimSession -ComputerName $computer -Credential $UNPASSWORD -OperationTimeoutSec 2
         $cpu = Get-CimInstance -Class Win32_Processor -CimSession $CimSession -ErrorAction Stop| Select-Object -ExpandProperty Name
         $ram = Get-CimInstance -Class Win32_PhysicalMemory -CimSession $CimSession | Measure-Object -Property Capacity -Sum | % { $_.Sum / 1GB }
         $gpu = Get-CimInstance -Class Win32_VideoController -CimSession $CimSession | Select-Object -ExpandProperty Name
@@ -58,6 +58,7 @@ foreach ($computer in $computers) {
         $results += [PSCustomObject]@{
             ComputerName = $computer
             CPU = ""
+            RAM = ""
             GPU = ""
             IP = ""
             LASTLOGON = $last_logon.LastLogonDate.ToString("dd/MM/yyyy")
@@ -82,5 +83,5 @@ Write-Host "Results saved to $filePath"
 $filePath = "C:\Temp\Workstation_List.csv"  # Specify your desired file path
 $results | Export-Csv -Path $filePath -NoTypeInformation
 
-Write-Host "Results saved to $filePath"
+#Write-Host "Results saved to $filePath"
 # Print to screen
